29df392d7fccc30d357a771aba10730154001419
erickssagala <erickssagala@gmail.com>
2022-08-03 00:22:48 +0300

    Bigbank

--- login/settings.py
+++ login/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 from .info import *
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -124,10 +125,19 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    "app/django/login/static",
-]
+
+
+
+# STATIC_URL = 'static/'
+# STATICFILES_DIRS = [
+#     "C:/Users/ERICK SSAGALA/Desktop/django/login/static",
+# ]
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- login/urls.py
+++ login/urls.py
@@ -17,13 +17,15 @@ from django.contrib import admin
 from django.urls import path,include
 
 from django.views.static import serve
-from django.conf.urls.static import url
+from django.conf.urls.static import re_path
+
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('authentication.urls')),
 
-    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+    re_path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
+    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
 
